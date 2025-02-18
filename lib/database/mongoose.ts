7c1ad95894712b488d673be093c5b7bb84b399be
imarwanmokhtar <marwanmokhtar000@gmail.com>
2025-02-18 20:35:01 +0200

    edit num7

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -11,26 +11,28 @@ interface GlobalWithMongoose {
   mongoose?: MongooseConnection;
 }
 
-let cached: MongooseConnection = (global as unknown as GlobalWithMongoose).mongoose;
+// Initialize the global mongoose connection if it doesn't exist
+const globalWithMongoose = global as unknown as GlobalWithMongoose;
 
-if(!cached) {
-  cached = (global as unknown as GlobalWithMongoose).mongoose = { 
-    conn: null, promise: null 
-  }
+if (!globalWithMongoose.mongoose) {
+  globalWithMongoose.mongoose = { conn: null, promise: null };
 }
 
+let cached: MongooseConnection = globalWithMongoose.mongoose;
+
 export const connectToDatabase = async () => {
-  if(cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-  cached.promise = 
-    cached.promise || 
-    mongoose.connect(MONGODB_URL, { 
-      dbName: 'imaginify', bufferCommands: false 
-    })
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: 'imaginify',
+      bufferCommands: false,
+    });
 
   cached.conn = await cached.promise;
 
   return cached.conn;
-}
+};
