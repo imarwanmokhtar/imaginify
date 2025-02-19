51fd410678e8661708fe160b9edb83f3821c90c7
imarwanmokhtar <marwanmokhtar000@gmail.com>
2025-02-19 21:28:16 +0200

    nah i'd win

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,18 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: ["/api/webhooks/clerk"],
-});
+// Middleware function
+export default clerkMiddleware();
 
+// Configuration for middleware
 export const config = {
   matcher: [
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
+
+    // Always run for API routes except for /api/webhooks/clerk
     '/(api|trpc)(.*)',
+
+    // Exclude /api/webhooks/clerk from Clerk middleware
+    '!/api/webhooks/clerk',
   ],
 };
